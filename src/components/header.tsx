bd1f0fc64e24cd4289d8ea52af4679469640249a
jose david <jose4125@gmail.com>
2021-05-01 15:28:56 -0500

    add netifly identity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
+import netlifyIdentity from 'netlify-identity-widget'
 
 interface Iprops {
   to: string
@@ -16,40 +17,47 @@ interface HeaderProps {
   siteTitle: string
 }
 
-const Header = ({ siteTitle }: HeaderProps) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }: HeaderProps) => {
+  useEffect(() => {
+    netlifyIdentity.init()
+  })
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <StaticImage
-        src="../images/gatsby-icon.png"
-        width={40}
-        alt="Gatsby logo"
-        placeholder="blurred"
-      />
-      <NavLink to="/">{siteTitle}</NavLink>
-      <NavLink to="/blog">blog</NavLink>
-      <NavLink to="/products">products</NavLink>
-      <div className="snipcart-summary snipcart-checkout">
-        <div>
-          <strong>my cart</strong>
-        </div>
-        <div>
-          <span className="snipcart-total-items"></span> items
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <StaticImage
+          src="../images/gatsby-icon.png"
+          width={40}
+          alt="Gatsby logo"
+          placeholder="blurred"
+        />
+        <NavLink to="/">{siteTitle}</NavLink>
+        <NavLink to="/blog">blog</NavLink>
+        <NavLink to="/products">products</NavLink>
+        <div data-netlify-identity-menu></div>
+        <div className="snipcart-summary snipcart-checkout">
+          <div>
+            <strong>my cart</strong>
+          </div>
+          <div>
+            <span className="snipcart-total-items"></span> items
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.defaultProps = {
   siteTitle: ``,
